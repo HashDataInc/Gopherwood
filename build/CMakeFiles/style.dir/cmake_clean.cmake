5b38b98c86f93588cebbc86262caa7f47df95918
Eric Wu <492960551@qq.com>
2017-12-04 15:26:42 +0800

    remove useless files

--- build/CMakeFiles/style.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/style"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/style.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
