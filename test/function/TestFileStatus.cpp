a232ba1e4c63c903633618857c52c3a49adca251
houliang <neuyilan@163.com>
2017-12-12 17:29:14 +0800

    add FileStatus unit test

---
+++ test/function/TestFileStatus.cpp
@@ -0,0 +1,72 @@
+//
+// Created by houliang on 12/5/17.
+//
+#ifndef TEST_HDFS_PREFIX
+#define TEST_HDFS_PREFIX "./"
+#endif
+
+#include "gtest/gtest.h"
+#include "core/gopherwood.h"
+#include "core/FileStatus.h"
+
+
+#include "../../src/core/FileSystemInter.h"
+
+
+using namespace Gopherwood;
+using namespace Gopherwood::Internal;
+
+class TestFileStatus : public ::testing::Test {
+public:
+
+    TestFileStatus() {
+        printf("Default Constructor of TestFileStatus");
+    }
+
+    ~TestFileStatus() {
+        printf("Default DeConstructor of TestFileStatus");
+    }
+
+protected:
+};
+
+TEST_F(TestFileStatus, serializeAndDeserializeFileStatus) {
+
+    vector<int32_t> blockIdVector = {1, 5, 6};
+    char *fileName = "filename-serializeFileStatus";
+    int lastBucket = 45;
+    int64_t endOffsetOfBucket = 54603;
+
+    FileStatus *beforeStatus = new FileStatus();
+    beforeStatus->setEndOffsetOfBucket(endOffsetOfBucket);
+    beforeStatus->setFileName(fileName);
+    beforeStatus->setBlockIdVector(blockIdVector);
+    beforeStatus->setLastBucket(lastBucket);
+
+    char* res = beforeStatus->serializeFileStatus();
+
+    FileStatus *afterStatus = beforeStatus->deSerializeFileStatus(res);
+    cout << "endOffsetOfBucket = " << afterStatus->getEndOffsetOfBucket() << endl;
+    cout << "lastBucket = " << afterStatus->getLastBucket() << endl;
+    cout << "fileName = " << afterStatus->getFileName() << endl;
+    cout << "res =" << res << endl;
+    for (int i = 0; i < afterStatus->getBlockIdVector().size(); i++) {
+        cout << "afterStatus[" << i << "]" << afterStatus->getBlockIdVector()[i] << endl;
+    }
+
+    ASSERT_EQ(afterStatus->getFileName(), fileName);
+    ASSERT_EQ(afterStatus->getEndOffsetOfBucket(), endOffsetOfBucket);
+    ASSERT_EQ(afterStatus->getLastBucket(), lastBucket);
+    ASSERT_EQ(afterStatus->getBlockIdVector(), blockIdVector);
+
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+#ifdef DATA_DIR
+    if (0 != chdir(DATA_DIR)) {
+        abort();
+    }
+#endif
+    return RUN_ALL_TESTS();
+}
