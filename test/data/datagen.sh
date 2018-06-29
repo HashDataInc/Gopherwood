4e06e575b0b4a5efdc378a6d6652c9bfb478e5f2
houliang <neuyilan@163.com>
2018-06-29 16:00:50 +0800

    change the .sh to english

--- test/data/datagen.sh
+++ test/data/datagen.sh
@@ -1,8 +1,7 @@
-1.在官网上(http://www.tpc.org/tpcds/ )去下载最新的：TPC-DS. 
+1.download the TPC-DS from http://www.tpc.org/tpcds
 
-2.解压: 下载的 zip 文件放在 linux 上解压,并进入他的 tools 目录.
+2.unzip the file, cd tools
 
-3.编译：make  这里需要linux安装上了 gcc , gcc c++, expect 等.
+3.make 
 
-4.生成数据：在tools目录下执行：./dsdgen -scale 5 -force (-force:会覆盖原来生成的data,否则不覆盖);生成的25个.dat 的数据文件.
-  -scale代表生成多大的数据
+4./dsdgen -scale 5 -force, -scale means generate how much data file
